cbc10110c016ff78ae506303948b944578a17856
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2024-01-15 22:03:47 +0300

    using static data to delete an employee

--- example-alpha/src/main/java/com/chief/examplealpha/client/EmployeeInit.java
+++ example-alpha/src/main/java/com/chief/examplealpha/client/EmployeeInit.java
@@ -1,8 +1,10 @@
 package com.chief.examplealpha.client;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +13,32 @@ import com.chief.examplealpha.entity.Employee;
 @Component
 public class EmployeeInit implements CommandLineRunner {
 
-    public List<Employee> creatEmployee() {
+    private List<Employee> employeeRepo = new ArrayList<>();
 
-        Employee mary = new Employee(21L, "Mary Mutua", "07****454*9", "101-uthiru", LocalDate.of(1999, 7, 7));
+    public void creatEmployee() {
+        Employee mary = new Employee(1L, "Mary Mutua", "07****454*9", "101-uthiru", LocalDate.of(1999, 7, 7));
         mary.setAge();
-        return List.of(mary);
+        Employee ken = new Employee(2L, "Ken Ruto", "07****454*9", "101-uthiru", LocalDate.of(1990, 7, 7));
+        ken.setAge();
+        employeeRepo.addAll(List.of(mary, ken));
+    }
+
+    public List<Employee> displayRepo() {
+
+        return employeeRepo;
+    }
+
+    public String deleteEmployee(Long id) {
+        Employee delEmployee = findEmployee(id);
+        employeeRepo.remove(delEmployee);
+        return "" + delEmployee.getName() + " has been deleted succesfully!!I";
+
+    }
+
+    public Employee findEmployee(Long id) {
+        return employeeRepo.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst().orElse(null);
     }
 
     @Override
--- example-alpha/src/main/java/com/chief/examplealpha/controller/EmployeeController.java
+++ example-alpha/src/main/java/com/chief/examplealpha/controller/EmployeeController.java
@@ -2,8 +2,11 @@ package com.chief.examplealpha.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chief.examplealpha.client.EmployeeInit;
@@ -12,11 +15,19 @@ import com.chief.examplealpha.entity.Employee;
 @RestController
 @RequestMapping(path = "api/employee")
 public class EmployeeController {
+    EmployeeInit init = new EmployeeInit();
+    {
+        init.creatEmployee();
+    }
 
     @GetMapping(path = "/getAll")
     public List<Employee> getEmployees(){
-        EmployeeInit init = new EmployeeInit();
-        return init.creatEmployee();
+        return init.displayRepo();
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public String deleteEmployee(@PathVariable Long id){
+        return init.deleteEmployee(id);
     }
 
     
